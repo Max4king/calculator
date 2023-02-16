32b0bc35545eb8198aaf47ddb731440f25e7ae5f
Max4King <ryan3311@hotmail.com>
2023-02-16 15:52:26 +0100

    my own version

---
+++ calculator.js
@@ -0,0 +1,11 @@
+/* Calculator.js
+    Basic Calculator with GUI
+    func + - * /
+*/
+
+function add(a,b) {return a + b;}
+function subtract(a,b) {return a - b;}
+function multiply(a,b) {return a * b;}
+function divide(a,b) {return a / b;}
+
+function operate(operator, num1, num2) {return operator(num1,num2);}
