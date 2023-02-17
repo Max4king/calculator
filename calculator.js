4ee98d07d772faa30df918413ca69f317d68e280
Max King <ryan3311@hotmail.com>
2023-02-17 17:10:42 +0100

    has some bug But is working

--- calculator.js
+++ calculator.js
@@ -8,11 +8,10 @@ function subtract(a,b) {return a - b;}
 function multiply(a,b) {return a * b;}
 function divide(a,b) {return a / b;}
 
-function operate(cur_operator, num1, num2) {
+function calculate(cur_operator, num1, num2) {
     console.log(cur_operator);
-    //let ans = cur_operator(num1,num2);
-    //return ans;
-    return 1;
+    let ans = cur_operator(num1,num2);
+    return ans;
 }
 
 const display = document.querySelector(".display");
@@ -25,6 +24,8 @@ let num_done = false;
 
 num_buttons.forEach ( (button) => {
     button.addEventListener("click", () => {
+        console.log(operator);
+        //console.log(operator === null);
         if (operator === null) {
             last_num = parseInt(last_num + button.value);
             display.textContent = last_num;
@@ -32,6 +33,7 @@ num_buttons.forEach ( (button) => {
             next_num = parseInt(next_num + button.value);
             display.textContent = next_num;
         }
+        console.log(calculate(operator, 10, last_num))
     })
 })
 const operators_buttons = document.querySelectorAll(".operator");
@@ -39,22 +41,24 @@ const operators_buttons = document.querySelectorAll(".operator");
 operators_buttons.forEach( button => {
     button.addEventListener( "click", () => {
         
-        if (operator === null && !num_done) {
+        if (operator === null && !num_done && button.value !== "operate") {
             operator = operator_check(button.value);
         }
         // figure out how to move function into a variable
         // Currently, it is giving a Nan instead of the function.
-        console.log("Operator :", operator)
+        //console.log("Operator :", operator)
+        
         if (operator !== null && !num_done) {
-            last_num = operate( operator, last_num, next_num);
+            last_num = calculate( operator, last_num, next_num);
             num_done = true;
             operator = null;
         }
 
         if (num_done) {
-            let temp = operator_check=(button.value);
+            let temp = operator_check(button.value);
             if (button.value === "operate" || temp !== null) {
                 display.textContent = last_num;
+                num_done = false;
             }
         }
     })
@@ -62,8 +66,7 @@ operators_buttons.forEach( button => {
 
 function operator_check(operator_value) {
     if(operator_value === "add") {
-        console.log("This work")
-        return add();
+        return add;
     }
     if(operator_value === "subtract") {
         return subtract;
@@ -74,6 +77,7 @@ function operator_check(operator_value) {
     if(operator_value === "multiply") {
         return multiply;
     }
+    console.log("This does not work");
     return null;
 }
 
--- calculator2.js
+++ calculator2.js
@@ -1,3 +1,8 @@
+/* Calculator.js
+    Basic Calculator with GUI
+    func + - * /
+*/
+
 function add(a,b) {return a + b;}
 function subtract(a,b) {return a - b;}
 function multiply(a,b) {return a * b;}
@@ -6,14 +11,80 @@ function divide(a,b) {return a / b;}
 function calculate(cur_operator, num1, num2) {return cur_operator(num1,num2);}
 
 const display = document.querySelector(".display");
+const clear_ac = document.querySelector(".clearing");
 const num_buttons = document.querySelectorAll(".num");
+const operators = document.querySelectorAll(".operator")
 
 let last_num = 0;
 let next_num = 0;
+let operator_func = undefined;
+let operator_used = false;
+let first_input = false;
+let second_input = false;
 
 num_buttons.forEach( button => {
     button.addEventListener ( "click", () => {
-        last_num = parseInt(last_num + button.value);
-        display.textContent = last_num;
+        console.log("num1: ",last_num);
+        console.log("num2: ", next_num);
+        if (!operator_used && !first_input){
+            console.log("Still in num1");
+            last_num = parseInt(last_num + button.value);
+            display.textContent = last_num;
+        } else {
+            next_num = parseInt(next_num + button.value);
+            display.textContent = next_num;
+            second_input = true;
+        }
     });
 });
+
+operators.forEach( button => {
+    button.addEventListener( "click", () => {
+        console.log(button.value, "is pressed");
+        if (button.value !== "operate" && !operator_used) {
+            operator_func = operator_check(button.value);
+            operator_used = true;
+            first_input = true;
+        } else{
+            if (second_input && operator_func) {
+                let ans = calculate(operator_func, last_num, next_num);
+                display.textContent = ans
+                last_num = ans;
+                operator_used = false;
+                operator_func = undefined;
+                next_num = 0;
+            }
+        }
+        
+    });
+});
+
+clear_ac.addEventListener( "click", () => {
+    last_num = 0;
+    next_num = 0;
+    operator_func = undefined;
+    operator_used = false;
+    first_input = false;
+    second_input = false;
+    display.textContent = 0;
+})
+
+
+
+
+function operator_check(operator_value) {
+    if(operator_value === "add") {
+        return add;
+    }
+    if(operator_value === "subtract") {
+        return subtract;
+    }
+    if(operator_value === "divide") {
+        return divide;
+    }
+    if(operator_value === "multiply") {
+        return multiply;
+    }
+    console.log("This does not work");
+    return;
+}
